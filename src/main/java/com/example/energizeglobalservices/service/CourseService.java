7c92e42b5a05f43cd6cdc14b88e92b72819eec8f
Nikolay Yakimov <96826786+Yakimov-Nikolay@users.noreply.github.com>
2022-07-29 16:22:54 +0300

    Add files via upload

--- src/main/java/com/example/energizeglobalservices/service/CourseService.java
+++ src/main/java/com/example/energizeglobalservices/service/CourseService.java
@@ -1,6 +1,8 @@
 package com.example.energizeglobalservices.service;
 
 import com.example.energizeglobalservices.model.dto.CourseDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.*;
 
@@ -9,11 +11,19 @@ public interface CourseService {
     List<CourseDTO> getAllCourses();
 
     Optional<CourseDTO> getCourseById(long id);
+
     void deleteCourse(long courseId);
 
     long createCourse(CourseDTO courseDTO);
 
 
     CourseDTO persistCourse(Long courseId, CourseDTO courseDTO);
+
+    Page<CourseDTO> getCourses(
+            int pageNo,
+            int pageSize,
+            String sortBy
+
+    );
 
 }
